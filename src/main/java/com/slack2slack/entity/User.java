57805f60dd15a0c09124b4defbf2e78a81fb0654
kpalese <kelly.palese@gmail.com>
2020-04-01 21:39:30 -0500

    Added sql dump for database, started User entity, updated pom.xml for Hibernate dependencies

---
+++ src/main/java/com/slack2slack/entity/User.java
@@ -0,0 +1,96 @@
+package com.slack2slack.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+
+/**
+ * Represents a Slack2Slack user
+ *
+ * @author Kelly Palese
+ */
+@Entity(name = "User")
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    /**
+     * Instantiates a new User (empty constructor)
+     */
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param name     the name
+     * @param password the password
+     */
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets password.
+     *
+     * @param password the password
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
