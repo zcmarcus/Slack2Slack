a7bff65c06ae05ded9cfaf41efca5c70a46f8b6e
kpalese <kelly.palese@gmail.com>
2020-04-06 08:57:41 -0500

    Updating cleandb.sql to insert three channels

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -14,4 +14,11 @@ values (1, 'admin', 1),
        (5, 'user', 4);
 
 delete from template;
-INSERT INTO `template` VALUES (1,'Basic','https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/256/examples.png','{\"email\": \"suesueserson@gmail.com\", \"last_name\": \"Sueserson\", \"first_name\": \"Sue\", \"display_name\": \"TestUser\"}',2);
+INSERT INTO `template`
+VALUES (1,'Basic','https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/256/examples.png','{\"email\": \"suesueserson@gmail.com\", \"last_name\": \"Sueserson\", \"first_name\": \"Sue\", \"display_name\": \"TestUser\"}',2);
+
+delete from channel;
+INSERT INTO channel
+VALUES (1, 'general', 'General class-wide information', 'Information pertaining to the entire class', 1),
+       (2, 'week-1', 'Week 1 content', 'Questions, tips, and tricks related to week 1 assignments', 1),
+       (3, 'week-2', 'week 2 content', 'Questions, tips, and tricks related to week 2 assignments', 1);
