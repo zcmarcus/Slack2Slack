041c87f2fa2985c1a7abdb5491d750cb258970d7
kpalese <kelly.palese@gmail.com>
2020-04-07 10:02:03 -0500

    Adding tests to TemplateDaoTest. Still need to add test for finding templates by user

--- src/test/java/com/slack2slack/persistence/TemplateDaoTest.java
+++ src/test/java/com/slack2slack/persistence/TemplateDaoTest.java
@@ -1,13 +1,16 @@
 package com.slack2slack.persistence;
 
+import com.slack2slack.entity.Role;
 import com.slack2slack.entity.Template;
 import com.slack2slack.entity.Channel;
+import com.slack2slack.entity.User;
 import com.slack2slack.test.util.Database;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TemplateDaoTest {
     GenericDao genericDao;
@@ -30,5 +33,74 @@ public class TemplateDaoTest {
         assertNotNull(retrievedTemplate);
         assertEquals("Basic", retrievedTemplate.getName());
         assertEquals("https://cdn0.iconfinder.com/data/icons/customicondesignoffice5/256/examples.png", retrievedTemplate.getIconUrl());
+    }
+
+    /**
+     * Verify successful update of a template
+     */
+    @Test
+    void updateSuccess() {
+        String newTemplateName = "Updated_Template";
+        Template templateToUpdate = (Template)genericDao.getById(1);
+        templateToUpdate.setName(newTemplateName);
+        genericDao.saveOrUpdate(templateToUpdate);
+        Template retrievedTemplate = (Template)genericDao.getById(1);
+        assertEquals(templateToUpdate, retrievedTemplate);
+    }
+
+    /**
+     * Verify successful insert of a template
+     */
+    @Test
+    void insertSuccess() {
+        GenericDao userDao = new GenericDao(User.class);
+        User user = (User)userDao.getById(2);
+
+        Template newTemplate = new Template("New Template", "www.fakeimage.com", "{\"email\": \"newemail@gmail.com\", \"last_name\": \"Jones\", \"first_name\": \"Joe\", \"display_name\": \"MrJones\"}", user);
+        int id = genericDao.insert(newTemplate);
+        assertNotEquals(0,id);
+        Template insertedTemplate = (Template)genericDao.getById(id);
+        assertEquals(newTemplate, insertedTemplate);
+    }
+
+    /**
+     * Verify successful delete of a template
+     */
+    @Test
+    void deleteSuccess() {
+        genericDao.delete(genericDao.getById(1));
+        assertNull(genericDao.getById(1));
+    }
+
+    /**
+     * Verify channels are deleted if the template is deleted
+     */
+    @Test
+    void deleteTemplateDeletesChannelsSuccess() {
+        genericDao.delete(genericDao.getById(1));
+
+        GenericDao channelDao = new GenericDao(Channel.class);
+        assertEquals(null, channelDao.getById(1));
+        assertEquals(null, channelDao.getById(2));
+        assertEquals(null, channelDao.getById(3));
+    }
+
+    /**
+     * Verify successful retrieval of all templates
+     */
+    @Test
+    void getAllSuccess() {
+        List<Template> templates = genericDao.getAll();
+        assertEquals(1, templates.size());
+    }
+
+    /**
+     * Verify successful get by property (equal match)
+     */
+    @Test
+    void getByPropertyEqualSuccess() {
+        //TODO: How to get templates by user? List<Template> templates = genericDao.getByPropertyEqual("user", "2");
+        //assertEquals(1, templates.size());
+        //assertEquals(2, templates.get(0).getId());
     }
 }
