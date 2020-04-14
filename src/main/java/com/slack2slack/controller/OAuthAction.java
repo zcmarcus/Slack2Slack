15b2b86e9d3bae9a6cf91b5506f8e33f786c613f
kpalese <kelly.palese@gmail.com>
2020-04-14 12:39:42 -0500

    OAuth basics are working! Need to verify if any additional parameters are needed in the session. Also need to update the scope from channels:manage to channels:write

--- src/main/java/com/slack2slack/controller/OAuthAction.java
+++ src/main/java/com/slack2slack/controller/OAuthAction.java
@@ -25,21 +25,30 @@ public class OAuthAction extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
 
         //Note that the temporary code expires after 10 minutes
         String tempSlackCode = req.getParameter("code");
+
+        System.out.println("***From OAuthAction temp code is: " + tempSlackCode);
+
         logger.info("******Temporary Slack code: " + tempSlackCode);
 
         OAuthDao oAuthDao = new OAuthDao();
         String accessToken = oAuthDao.getOAuthResponse(tempSlackCode).getAccessToken();
+
+        //TODO: If null set an error?
+
+        System.out.println("***From OAuthAction access token is: " + accessToken);
+
+
         logger.info("******Access token: " + accessToken);
         //TODO: grab any other parameters we need for future slack api requests
 
         //Add the necessary OAuth tokens to the session
-        HttpSession session = req.getSession();
         session.setAttribute("accessToken", accessToken);
 
-        //TODO: Put the access token in the database
+        //TODO: Put the access token in the database (possibly)
 
         //Forward to the home page
         RequestDispatcher dispatcher = req.getRequestDispatcher("/index.jsp");
--- src/test/java/com/slack2slack/persistence/OAuthDaoTest.java
+++ src/test/java/com/slack2slack/persistence/OAuthDaoTest.java
@@ -14,7 +14,7 @@ public class OAuthDaoTest implements PropertiesLoader {
     @Test
     //This is testing that the app_id in the response is as expected, since the app_id is a static parameter.
     //tempSlackCode is not a static parameter and will need to be manually added to the test
-    void getOAuthResponseSuccess() throws Exception {
+    void getOAuthResponseSuccess() {
         OAuthDao dao = new OAuthDao();
 
         String expectedApp_id = "";
@@ -28,7 +28,7 @@ public class OAuthDaoTest implements PropertiesLoader {
         }
 
         //IMPORTANT: Temporary slack code is generated after logging into Slack2Slack. You can find it in the url. It expires after 10 minutes.
-        assertTrue(dao.getOAuthResponse("").isOk());
+        assertEquals(true, dao.getOAuthResponse("").isOk());
         assertEquals(expectedApp_id, dao.getOAuthResponse("").getAppId());
     }
 }
