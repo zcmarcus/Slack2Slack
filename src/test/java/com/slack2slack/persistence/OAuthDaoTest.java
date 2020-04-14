de4d115975ed693ea216de5e716bfababd3a3feb
kpalese <kelly.palese@gmail.com>
2020-04-13 21:37:42 -0500

    Implemented properties in OAuthDaoTest. ISSUE: When testing, I am receiving an invalid client_id response from Slack

--- src/test/java/com/slack2slack/persistence/OAuthDaoTest.java
+++ src/test/java/com/slack2slack/persistence/OAuthDaoTest.java
@@ -1,24 +1,40 @@
 package com.slack2slack.persistence;
 
+import com.slack2slack.util.PropertiesLoader;
 import org.junit.jupiter.api.Test;
 import javax.ws.rs.core.MediaType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import java.util.Properties;
 
 /*
 * //TODO: JAVADOC
  */
-public class OAuthDaoTest {
+public class OAuthDaoTest implements PropertiesLoader {
     @Test
     void getSlackUserTokenSuccess() throws Exception {
-        String baseURL = "https://slack.com/api/oauth.v2.access?code=";
+        String oAuthAccessMethod = "";
         //For testing purposes, you can get this after logging into our Slack2Slack app and then grabbing the code parameter from the URL
-        String tempSlackCode = "1025804317248.1059683487554.1e220b48d546805b00304853c2afd2908cad110645cfa2bf8573dbe48d0cd292";
+        String tempSlackCode = "";
+        String client_id = "";
+        String client_secret = "";
+
+        //Load the Slack authentication properties from the properties file
+        try {
+            Properties properties = loadProperties("/slack.secrets.properties");
+            oAuthAccessMethod = properties.getProperty("oAuthAccessMethod");
+            client_id = properties.getProperty("client_id");
+            client_secret = properties.getProperty("client_secret");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         Client client = ClientBuilder.newClient();
         WebTarget target =
-                client.target(baseURL + tempSlackCode);
+                client.target(oAuthAccessMethod + "?code=" + tempSlackCode + ",client_id=" + client_id + ",client_secret=" + client_secret);
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         assertEquals("???", response);
     }
